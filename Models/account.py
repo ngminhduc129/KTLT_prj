53c2ef1f94b58fe09f1cff6a24ee9311be6785c6
dnb1010 <dongngocbao101026@gmail.com>
2026-05-20 15:36:10 +0700

    abc

--- Models/account.py
+++ Models/account.py
@@ -1,8 +1,7 @@
 from datetime import datetime, date
 class Account:
-    def __init__(self, account_id, full_name, balance, pin, user_id, password, status, create_at, time_created=None):
+    def __init__(self, account_id, balance, pin, user_id, password, status, create_at, time_created=None):
         self.account_id = account_id
-        self.full_name = full_name
         self.balance = float(balance)
         self.pin = pin
         self.user_id = user_id
@@ -23,7 +22,6 @@ class Account:
     # In ra 
     def display_info(self):
         print("Số tài khoản: ", self.account_id)
-        print("Họ và tên: ", self.full_name)
         print("Số dư tài khoản: ", self.balance)
         print("Trạng thái: ", self.status)
         print("Chi nhánh: ", self.create_at)
@@ -31,7 +29,7 @@ class Account:
     
     # Nối các thuộc tính từ tham số đầu vào thành một chuỗi
     def to_file_string(self):
-        return f"{self.account_id}|{self.full_name}|{self.balance}|{self.pin}|{self.user_id}|{self.password}|{self.status}|{self.create_at}|{self.time_created}"
+        return f"{self.account_id}|{self.balance}|{self.pin}|{self.user_id}|{self.password}|{self.status}|{self.create_at}|{self.time_created}"
     
     # Khi thực hiện một chức năng load data từ file text ta sẽ tách các thuộc tính ra riêng biệt
     @staticmethod
@@ -39,12 +37,11 @@ class Account:
         data = line.strip().split('|')
         return Account(
             account_id=data[0],
-            full_name=data[1],
-            balance=data[2],
-            pin=data[3],
-            user_id=data[4],
-            password=data[5],
-            status=data[6],
-            create_at=data[7],
-            time_created=data[8]           
+            balance=data[1],
+            pin=data[2],
+            user_id=data[3],
+            password=data[4],
+            status=data[5],
+            create_at=data[6],
+            time_created=data[7]           
         )
