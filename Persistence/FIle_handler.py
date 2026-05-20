88c7903d11bb00b19a22d72b28021c9de95549c4
chien456 <nguyenquangchien206.na@gmail.com>
2026-05-20 19:32:02 +0700

    Add File_handler

---
+++ Persistence/FIle_handler.py
@@ -0,0 +1,15 @@
+import os
+
+class FileHandler:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read_file(self):
+        if not os.path.exists(self.file_path):
+            return None
+        with open(self.file_path, 'r', encoding="utf-8") as file:
+            return file.read()
+
+    def write_file(self, data):
+        with open(self.file_path, 'w', encoding="utf-8") as file:
+            file.write(data)  
